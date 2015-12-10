9c54ccd3206babf5a7549b9ae9148eac4d367169
anand.kushwaha <anand.kushwaha@paytm.com>
2015-12-10 21:05:17 +0530

    layout

---
+++ server/src/sign.go
@@ -0,0 +1,7 @@
+import(
+	"log"
+)
+func Authorize(email, password string) (bool, error){
+	//TODO:
+	return false
+}
---
+++ server/src/signup.go
@@ -0,0 +1,13 @@
+import(
+	"log"
+)
+type UserData struct{
+	name string
+	email string
+	phno string
+	password string
+}
+func Signup(data *UserData)(err error){
+	//TODO 
+	
+}
